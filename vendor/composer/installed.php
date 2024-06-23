e6d7879440fc0b06ec938287e216c8750dc6534a
ashikXpress <ctashiqkhan@gmail.com>
2024-06-23 11:17:41 +0600

    added require

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ashik/laravel-decimal-to-word',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '875a60a408187c0e8cd116595c3e408b05b0e6a2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ashik/laravel-decimal-to-word' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '875a60a408187c0e8cd116595c3e408b05b0e6a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
